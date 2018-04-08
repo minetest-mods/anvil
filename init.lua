d394b8b8252a3ec82d298a66802108f3e27dd9ec
FaceDeer <derksenmobile@gmail.com>
2018-04-08 17:06:19 -0600

    tweak recipe to remove conflict with steel sign

--- init.lua
+++ init.lua
@@ -368,6 +368,6 @@ minetest.register_craft({
 	recipe = {
                 {"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
                 {"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-                {'',                   "group:stick",      ''                   } }
+                {"group:stick", '', ''} },
 })
 
